937b5a0c4ee742f9e4870ba9a3b1d2250ec2ca84
OliverEC <oliverec04@gmail.com>
2023-11-25 02:12:01 +0100

    wdads

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -21,6 +21,8 @@ fn fruit_basket() -> HashMap<String, u32> {
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
+
+
     
     // TODO: Put more fruits in your basket here.
 
